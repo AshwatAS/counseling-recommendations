4b35221b2371b203e69544bd78b1ebb8d20cdc22
AshwatAS <63542367+AshwatAS@users.noreply.github.com>
2024-12-11 15:08:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ st.info("We help you decide what you should study in the future with the help of
 # Collect the name here
 
 # Start with data collection
-
+st.header("Try and answer as accurately as possible! Don't worry these questions are helping you, not deciding your future.")
