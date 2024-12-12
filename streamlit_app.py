fdcc636ac8da8d146134db6460e8db25777cff47
AshwatAS <63542367+AshwatAS@users.noreply.github.com>
2024-12-12 12:47:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,4 @@ st.info("We help you decide what you should study in the future with the help of
 st.subheader("Try and answer as accurately as possible! Don't worry these questions are helping you, not deciding your future.")
 preferred_environment=st.selectbox("What is your preferred work environment?",("Teaching and Training", "Remote/Work from Home", "On-site Industrial Work", "Desk Job", "Fieldwork", "Research Lab", "Creative Studio"))
 salary_expect=st.slider("How much annual income do you expect from your job in the future?(INR)",400000,1500000,step=50000)
-st.title("heheiuier")
+
